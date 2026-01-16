07cba710aa6dbdceb37a9a12cdcb7e76e7452b60
Vaios Anastasiadis <vaios.anastasiadis@agileactors.com>
2026-01-16 15:36:00 +0200

    Add fe services for booking

--- tenant-website/src/queries/index.ts
+++ tenant-website/src/queries/index.ts
@@ -23,6 +23,42 @@ export const getTenant = (tenant: string): Promise<any> => {
   });
 };
 
+export const getTenantServices = (tenant: string): Promise<any> => {
+  return makeRequest({
+    method: "GET",
+    url: `tenants/${tenant}/services`,
+  });
+};
+
+export const getTenantAvailability = (
+  tenant: string,
+  date: string,
+  serviceId: number
+): Promise<any> => {
+  return makeRequest({
+    method: "GET",
+    url: `tenants/${tenant}/availability?date=${date}&serviceId=${serviceId}`,
+  });
+};
+
+export const addCustomerAppointment = (
+  tenant: string,
+  date: string,
+  serviceId: number,
+  selectedSlot: any
+): Promise<any> => {
+  return makeRequest({
+    method: "POST",
+    url: `tenants/${tenant}/appointments`,
+    body: JSON.stringify({
+      tenant,
+      serviceId,
+      date,
+      startTime: selectedSlot,
+    }),
+  });
+};
+
 export const getHeroDetails = ({
   tenantId,
   sectionId,
